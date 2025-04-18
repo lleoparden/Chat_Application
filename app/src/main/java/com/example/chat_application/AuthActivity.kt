e09c5a225dad8130005aa4b52ede191e46cadcb2
Yassin Marwan <Yassin.marwann@gmail.com>
2025-04-18 19:54:07 +0200

    Made Layout  XML Files For login_page.xml, signup_page.xml and authentication_page.xml
    FRONTEND ONLY WITH NO FUNCTIONALITY.

--- app/src/main/java/com/example/chat_application/AuthActivity.kt
+++ app/src/main/java/com/example/chat_application/AuthActivity.kt
@@ -1,22 +1,24 @@
 package com.example.chat_application
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import android.widget.Button
+import android.widget.TextView
 
 
 class AuthActivity : AppCompatActivity() {
 
-    private lateinit var toMainPage : Button
+    private lateinit var toMainPage : TextView
 
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.auth_mainpage)
+        setContentView(R.layout.login_page)
 
-        toMainPage = findViewById(R.id.button)
+        toMainPage = findViewById(R.id.switchSignInButton)
         toMainPage.setOnClickListener{
             startActivity(Intent(this, MainActivity::class.java))
             finish()
