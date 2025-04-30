fcda13d62a2f8cc6b1646a1bd60dd4689574bb53
Yassin Wahb <ym8142004@gmail.com>
2025-05-01 00:40:23 +0300

    CodeRestriction Done

--- app/src/main/java/com/example/chat_application/AuthActivity.kt
+++ app/src/main/java/com/example/chat_application/AuthActivity.kt
@@ -296,6 +296,7 @@ class AuthActivity : AppCompatActivity() {
         val hasSpecialChar = password.any { !it.isLetterOrDigit() }
 
         return hasUppercase && hasSpecialChar
+
     }
 
     private fun validatePassword(password: String, passwordField: EditText) {
