015cec44d46a6b0974395e1fa5710d367d6271d4
Mostafa <mustafa.kafrawy123@gmail.com>
2025-05-03 00:01:55 +0300

    cleaning the code

--- app/src/main/java/com/example/chat_application/AddNewGroupActivity.kt
+++ app/src/main/java/com/example/chat_application/AddNewGroupActivity.kt
@@ -14,6 +14,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.example.chat_application.adapters.SelectedMemberAdapter
 import com.example.chat_application.adapters.UserAdapter
 import com.example.chat_application.dataclasses.Chat
 import com.example.chat_application.dataclasses.UserData
--- app/src/main/java/com/example/chat_application/adapters/SelectedMemberAdapter.kt
+++ app/src/main/java/com/example/chat_application/adapters/SelectedMemberAdapter.kt
@@ -1,4 +1,4 @@
-package com.example.chat_application
+package com.example.chat_application.adapters
 
 import android.view.LayoutInflater
 import android.view.View
@@ -7,6 +7,7 @@ import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.example.chat_application.R
 import com.example.chat_application.dataclasses.UserData
 
 class SelectedMemberAdapter(
